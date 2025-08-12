f1c0a6c82ed9d8c37cabee556711e63d22e162d1
Melikyin <melik-sayin.ilkay@it-novum.com>
2025-08-12 10:37:03 +0200

    Update openmeteo_to_thingsboard.py

--- openmeteo_to_thingsboard.py
+++ openmeteo_to_thingsboard.py
@@ -1,40 +1,34 @@
 import requests
 from datetime import datetime
-import time
+import os
 
-ACCESS_TOKEN = "X5AQByKOKs83CvGiouIL"
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]  # Holt Token aus GitHub Secret
 
-def send_weather():
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": 52.52,
-        "longitude": 13.41,
-        "hourly": "temperature_2m,rain,wind_speed_80m,wind_direction_80m,relative_humidity_2m",
-        "timezone": "auto"
-    }
-    weather = requests.get(url, params=params).json()
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": 52.52,
+    "longitude": 13.41,
+    "hourly": "temperature_2m,rain,wind_speed_80m,wind_direction_80m,relative_humidity_2m",
+    "timezone": "auto"
+}
 
-    temp = weather["hourly"]["temperature_2m"][0]
-    rain = weather["hourly"]["rain"][0]
-    wind_speed = weather["hourly"]["wind_speed_80m"][0]
-    wind_dir = weather["hourly"]["wind_direction_80m"][0]
-    humidity = weather["hourly"]["relative_humidity_2m"][0]
+weather = requests.get(url, params=params).json()
 
-    thingsboard_url = f"https://iot-demo.bda-itnovum.com/api/v1/{ACCESS_TOKEN}/telemetry"
-    payload = {
-        "temperature": temp,
-        "rain": rain,
-        "wind_speed": wind_speed,
-        "wind_direction": wind_dir,
-        "humidity": humidity,
-        "timestamp": datetime.now().isoformat()
-    }
+temp = weather["hourly"]["temperature_2m"][0]
+rain = weather["hourly"]["rain"][0]
+wind_speed = weather["hourly"]["wind_speed_80m"][0]
+wind_dir = weather["hourly"]["wind_direction_80m"][0]
+humidity = weather["hourly"]["relative_humidity_2m"][0]
 
-    response = requests.post(thingsboard_url, json=payload)
-    print(datetime.now(), "Status:", response.status_code)
+thingsboard_url = f"https://iot-demo.bda-itnovum.com/api/v1/{ACCESS_TOKEN}/telemetry"
+payload = {
+    "temperature": temp,
+    "rain": rain,
+    "wind_speed": wind_speed,
+    "wind_direction": wind_dir,
+    "humidity": humidity,
+    "timestamp": datetime.now().isoformat()
+}
 
-# Endlosschleife: jede Stunde
-while True:
-    send_weather()
-    time.sleep(3600)  # 3600 Sekunden = 1 Stunde
-
+response = requests.post(thingsboard_url, json=payload)
+print("Status:", response.status_code, "Antwort:", response.text)
